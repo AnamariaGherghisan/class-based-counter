b0046149e2722d214096b52b241d437820e5047a
Anamaria Gherghisan <sanduanamaria44@yahoo.com>
2023-06-07 02:28:34 +0100

    add state to counter

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ export const Banner = () => {
   return (
   <div className="jumbotron">
       <div className="title">React Counter</div>
-      <div classNmae="sub-title">
+      <div className="sub-title">
         Please use the buttons to interact with your counter
         </div>
   </div>
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,19 +3,47 @@ import { Component } from "react";
 export class Counter extends Component {
   constructor(props) {
     super(props);
-  }
-  
+    
+    this.state = {count: 0};
+    }
+
+  handleDecrement = () => { 
+   //step1:console.log("handleDecrement")-for testing purposes to check if the button is working;
+   //step2:update the state
+  if(this.state.count>0){
+  this.setState({
+    count: this.state.count - 1,
+  });
+}
+};
+
+
+handleIncrement = () => {
+    //step1:console.log("handleIncrement");
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  handleReset = () => {
+    //step1:console.log("handleReset");
+    this.setState({
+      count: 0,
+    });
+  };
+
   render() {
-    return  (
+    console.log("render");
+    return (
     <div className="border m-3 p-4 rounded">
-    <div className="sub-title text-center mb-3 ">Counter: 0</div>
+    <div className="sub-title text-center mb-3 ">Counter: {this.state.count}</div>
     <div className= "d-grid gap-2 col-6 mx-auto">
-      <button className="btn btn-success text-center">Decrement</button>
-      <button className="btn btn-danger text-center">Reset</button>
-      <button className="btn btn-success text-center">Increment</button>
+      <button className="btn btn-success text-center" onClick={this.handleDecrement}>Decrement</button>
+      <button className="btn btn-danger text-center" onClick={this.handleReset}>Reset</button>
+      <button className="btn btn-success text-center" onClick={this.handleIncrement}>Increment</button>
     </div>
     </div>
     );
 
   }
-  }
+}
