d92acf5ff64583b2deb59ed0da3031a1fa732116
Anamaria Gherghisan <sanduanamaria44@yahoo.com>
2023-06-07 01:24:04 +0100

    complete styled counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,7 +6,16 @@ export class Counter extends Component {
   }
   
   render() {
-    return <div>Counter</div>;
+    return  (
+    <div className="border m-3 p-4 rounded">
+    <div className="sub-title text-center mb-3 ">Counter: 0</div>
+    <div className= "d-grid gap-2 col-6 mx-auto">
+      <button className="btn btn-success text-center">Decrement</button>
+      <button className="btn btn-danger text-center">Reset</button>
+      <button className="btn btn-success text-center">Increment</button>
+    </div>
+    </div>
+    );
 
   }
   }
